1a3b355a78ca9a3bd5427ac242a9dabb4a119dd0
MCannon33 <marialcannon@gmail.com>
2021-04-18 16:01:54 -0400

    add local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,7 +68,7 @@ function endGame() {
 //local storage
 function newScore() {
   localStorage.setItem("highscore", score);
-  localStorage.setItem("highscoreName", document.getElementById("name").value);
+  localStorage.setItem("highscoreName", document.getElementById(`name`).value);
   getScore();
 }
 
